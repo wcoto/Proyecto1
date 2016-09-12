c12ed4d25e3227b55012b57549bccec546e245bf
manu3193 <manu3193@gmail.com>
2016-09-12 02:12:31 -0600

    Add suported operations definition

---
+++ aluOps.h
@@ -0,0 +1,15 @@
+//Alu operations
+
+`define aluNoop 4'b0000
+`define aluMov  4'b0001
+`define aluAdd  4'b0010
+`define aluSub  4'b0011
+`define aluXor 4'b0100
+`define aluAnd  4'b0101
+`define aluOr  4'b0110
+`define aluSL  4'b0111
+`define aluSR 4'b1000
+`define aluRotL 4'b1001
+`define aluRotR 4'b1010
+
+
---
+++ memOps.h
@@ -0,0 +1,7 @@
+//Mem operations
+
+`define memNoop 3'b000
+`define strv_r 3'b001
+`define strv_i 3'b010
+`define ldv_i 3'b011
+`define ldv_r 3'b100
