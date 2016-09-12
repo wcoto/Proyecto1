3c973ec10691ee2927d80be59a954e82eb2dc847
manu3193 <manu3193@gmail.com>
2016-09-12 02:13:02 -0600

    opcode definition

---
+++ opcodes.h
@@ -0,0 +1,20 @@
+//Opcodes of instructions used in SEVERE Processor
+
+`define NOOP 5'b00000
+`define ADD  5'b00001
+`define SUB  5'b00010
+`define XOR  5'b00011
+`define AND  5'b00100
+`define ORR  5'b00101
+`define LDV_I 5'b00110
+`define LDV_R 5'b00111
+`define STR_I 5'b01000
+`define STR_R 5'b01001
+`define MOVS  5'b01010
+`define MOVV  5'b01011
+`define SLV   5'b01100
+`define SRV   5'b01101
+`define ROL   5'b01110
+`define ROR   5'b01111
+
+
