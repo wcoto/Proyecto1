c1c75b465d779a58c66c4f3b84e85bbfb27ad4c4
Kevin <kevgiso@hotmail.com>
2016-09-15 17:33:03 -0600

    Add data memory, register files with corrections.

--- DataMemory.v
+++ DataMemory.v
@@ -1,43 +1,62 @@
-module DataMemory #(
-parameter  MEM_SIZE=262144,
-			  BIT_NUMBER=32,
-		     VECTOR_SIZE=64
-)(
-input clk,
-input enable,
-input write_enable,
-input [BIT_NUMBER-1:0] address,
-input [VECTOR_SIZE-1:0] data_in,
-output reg [VECTOR_SIZE-1:0] data_out
+//module DataMemory #(
+//parameter  ADDR_WIDTH=32,
+//		     VECTOR_SIZE=64
+//)(
+//input clk,
+//input write_enable,
+//input [ADDR_WIDTH-1:0] address,
+//input [VECTOR_SIZE-1:0] data_in,
+//output reg [VECTOR_SIZE-1:0] data_out
+//);
+//
+//reg [VECTOR_SIZE-1:0] DataMemory [2**ADDR_WIDTH-1:0];
+// 
+//always @(posedge clk)
+//	if (enable)
+//		begin
+//			if (write_enable)
+//				begin
+//					DataMemory[address] <= data_in;
+//					data_out <= data_in;
+//				end
+//		end
+//	else
+//		begin
+//			data_out <= DataMemory[address];
+//		 end
+//
+//endmodule 
+
+module DataMemory 
+#(parameter DATA_WIDTH=64, parameter ADDR_WIDTH=32)
+(
+	input [(DATA_WIDTH-1):0] data_in,
+	input [(ADDR_WIDTH-1):0] addr, addr_2,
+	input we, clk,
+	output [(DATA_WIDTH-1):0] data_out, data_out2
 );
 
-reg [VECTOR-SIZE:0] DataMemory [0:MEM_SIZE-1];
-integer i;
- 
-always @(posedge clk)
+	// Declare the RAM variable
+	reg [DATA_WIDTH-1:0] ram[2**ADDR_WIDTH-1:0];
+
+	// Variables to hold the registered read addresses
+	reg [ADDR_WIDTH-1:0] addr_reg;
+	reg [ADDR_WIDTH-1:0] vga_reg;
+
+	always @ (posedge clk)
 	begin
-	if (reset)
-			begin
-				for(i=0; i < MEM_SIZE; i = i + 1)
-					begin
-						DataMemory[i]<= 64'bx;
-					end
-			end
-	else if (enable)
-		begin
-		if (write_enable)
-			begin
-				DataMemory[address] <= data_in;
-			end
-		else
-		 begin
-		 data_out <= DataMemory[address];
-		 end
-			end
+		// Write
+		if (we)
+			ram[addr] <= data_in;
 
+		addr_reg <= addr;
+		vga_reg <= addr_2;
+	end
 
-
-
-
-
-endmodule 
+	// Continuous assignment implies read returns NEW data.
+	// This is the natural behavior of the TriMatrix memory
+	// blocks in Single Port mode.  
+	assign data_out = ram[addr_reg];
+	assign data_out2 = ram[vga_reg];
+ 
+endmodule
