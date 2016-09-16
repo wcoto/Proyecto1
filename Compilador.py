373e08756a945ad37017bf5a6f1c68f7ab7cfa23
wcoto <wagner.coto.m@gmail.com>
2016-09-16 14:09:01 -0600

    Compilador listo

--- Compilador.py
+++ Compilador.py
@@ -9,37 +9,39 @@ def patron(instruccion):
 
     if (matchObj):
         function = matchObj.group(1)
-        if ((function == "ADDV")  | (function == "SUBV") | (function == "XORV") | (function == "ANDV") | (function == "ORV")):
+        if (function == "NOP"):
+            return "00000000000000000000000000000000"
+        elif ((function == "ADDV")  | (function == "SUBV") | (function == "XORV") | (function == "ANDV") | (function == "ORV")):
             operand1 = matchObj.group(4)
             operand2 = matchObj.group(7)
             operand3 = matchObj.group(10)
-            
+
             opcode = obtenerOpCode(function)
             Vk     = obtenerBinario(int(operand1), '4')
             Vw     = obtenerBinario(int(operand2), '4')
             Vz     = obtenerBinario(int(operand3), '4')
-            
-            return opcode +" "+ Vk +" "+ Vw +" "+ Vz + " 000000000000000"
+
+            return opcode + Vk + Vw + Vz + "000000000000000"
         elif ((function == "LDV") | (function == "STV")):
             operand1 = matchObj.group(4)
             operand2 = matchObj.group(7)
             if (matchObj.group(6) == "R"):
                 operand3 = matchObj.group(10)
-            
+
                 opcode = obtenerOpCode(function+"r")
                 Vk  = obtenerBinario(int(operand1), '4')
                 Rw  = obtenerBinario(int(operand2), '4')
                 imm = obtenerBinario(int(operand3, 16), '19')
 
-                return opcode +" "+ Vk +" "+ Rw +" "+imm
+                return opcode + Vk + Rw + imm
             else: #immediate
                 opcode = obtenerOpCode(function+"i")
 
                 Vk = obtenerBinario(int(operand1), '4')
                 Rw = obtenerBinario(0, '4')
                 imm = obtenerBinario(int(operand2, 16), '19')
-                
-                return opcode +" "+ Vk +" "+ Rw +" "+imm
+
+                return opcode + Vk + Rw +imm
         elif ((function == "MOVS") | (function == "MOVV")):
             operand1 = matchObj.group(4)
             operand2 = matchObj.group(7)
@@ -49,39 +51,39 @@ def patron(instruccion):
                 Rw  = obtenerBinario(int(operand2), '4')
                 imm = obtenerBinario(0, '19')
 
-                return opcode +" "+ Rk +" "+ Rw +" "+imm
+                return opcode + Rk + Rw + imm
             else: # immediate
                 opcode = obtenerOpCode(function+"i")
 
                 Rk  = obtenerBinario(int(operand1), '4')
                 Rw  = obtenerBinario(0, '4')
                 imm = obtenerBinario(int(operand2, 16), '19')
-                
-                return opcode +" "+ Rk +" "+ Rw +" "+imm 
+
+                return opcode + Rk + Rw + imm 
         else: # desplazamiento
             operand1 = matchObj.group(4);
             operand2 = matchObj.group(7);
             operand3 = matchObj.group(10)
-            
-            if ((matchObj.group(9) == "R")):                
 
-                opcode = obtenerOpCode(function)                
+            if ((matchObj.group(9) == "R")):
+
+                opcode = obtenerOpCode(function)
                 Vk     = obtenerBinario(int(operand1), '4')
                 Vw     = obtenerBinario(int(operand2), '4')
                 Rz     = obtenerBinario(int(operand3), '4')
                 bitDes = "0"
                 imm    = obtenerBinario(0, '14')
 
-                return opcode +" "+ Vk +" "+ Vw +" "+ Rz +" "+ bitDes +" "+ imm
+                return opcode + Vk + Vw + Rz + bitDes + imm
             else: #inmediato
-                opcode = obtenerOpCode(function)                
+                opcode = obtenerOpCode(function)
                 Vk     = obtenerBinario(int(operand1), '4')
                 Vw     = obtenerBinario(int(operand2), '4')
                 Rz     = obtenerBinario(0, '4')
                 bitDes = "1"
                 imm    = obtenerBinario(int(operand3), '14')
 
-                return opcode +" "+ Vk +" "+ Vw +" "+ Rz +" "+ bitDes +" "+ imm
+                return opcode + Vk + Vw + Rz + bitDes + imm
     else:
         print ("No match")
 
@@ -110,7 +112,7 @@ def obtenerOpCode(function):
                "ROTLV" : "10000",
                "ROTRV" : "10001"}
     return options[function]
-    
+
 def abrirArchivo():
     archivoEnsamblador = open('Codigo.txt', 'r')
     archivoBinario     = open('iMem.txt', 'w')
@@ -121,8 +123,8 @@ def abrirArchivo():
             break
         binario = patron(linea) + "\n"
         archivoBinario.write(binario)
-            
+
     archivoEnsamblador.close()
-    archivoBinario.close()       
+    archivoBinario.close()
 
 abrirArchivo()
